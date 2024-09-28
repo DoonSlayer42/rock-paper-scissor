1794d81c43717134ecaab6ef8938aaae147a86c9
DoonSlayer42 <internetsaathihwgo6040@gmail.com>
2024-09-28 20:10:17 +0530

    reverting to a previous version

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // 0->Rock, 1->Paper, 2->Scissor
 
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return choice;
 }
 
-// console.log(getComputerChoice());
-
 function getHumanChoice() {
-    let choice = prompt("Enter your choice (rock, paper, scissor): ", "rock");
+    let choice = prompt("Enter your choice (rock, paper, scissor): ");
+    choice = choice.toLowerCase();
     if(choice === "rock") {
         return 0;
     }
@@ -18,6 +19,48 @@ function getHumanChoice() {
     return 2;
 }
 
-console.log(getHumanChoice());
 
-// let userResponse = prompt("Enter your choice (rock, paper or scissor): ", "rock");
+function playRound(humanSelection, computerSelection) {
+    if(humanSelection === 0) {
+        if(computerSelection === 1) {
+            computerScore++;
+        }
+        else if(computerSelection === 2) {
+            humanScore++;
+        }
+    }
+    else if(humanSelection === 1) {
+        if(computerSelection === 0) {
+            humanScore++;
+        }
+        else if(computerSelection === 2) {
+            computerScore++;
+        }
+    }
+    else {
+        if(computerSelection === 0) {
+            computerScore++;
+        }
+        else if(computerSelection === 1) {
+            humanScore++;
+        }
+    }
+}
+
+for(let i = 0; i < 5; i++) {
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+}
+
+console.log(humanScore + ' : ' + computerScore);
+
+if(humanScore > computerScore) {
+    console.log("You won!");
+}
+else if(humanScore < computerScore) {
+    console.log("Game over!");
+}
+else {
+    console.log("It's a draw!");
+}
