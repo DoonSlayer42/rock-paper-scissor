39c47eda8d13f8cbd3946b28e0adfbebece1ca85
DoonSlayer42 <internetsaathihwgo6040@gmail.com>
2024-09-28 20:17:56 +0530

    Adding styles and logic with some UI.
    The site looks a bit more user friendly.

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
 // 0->Rock, 1->Paper, 2->Scissor
 
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return choice;
 }
 
-// console.log(getComputerChoice());
-
-function getHumanChoice() {
-    let choice = prompt("Enter your choice (rock, paper, scissor): ", "rock");
-    if(choice === "rock") {
-        return 0;
+function playRound(humanSelection) {
+    let computerSelection = getComputerChoice();
+    if(humanSelection === 0) {
+        if(computerSelection === 1) {
+            computerScore++;
+        }
+        else if(computerSelection === 2) {
+            humanScore++;
+        }
     }
-    else if(choice === "paper") {
-        return 1;
+    else if(humanSelection === 1) {
+        if(computerSelection === 0) {
+            humanScore++;
+        }
+        else if(computerSelection === 2) {
+            computerScore++;
+        }
     }
-    return 2;
+    else {
+        if(computerSelection === 0) {
+            computerScore++;
+        }
+        else if(computerSelection === 1) {
+            humanScore++;
+        }
+    }
 }
 
-console.log(getHumanChoice());
+/* * * * * * *
 
-// let userResponse = prompt("Enter your choice (rock, paper or scissor): ", "rock");
+Adding event listeners
+
+* * * * * * */
+
+const btns = document.querySelectorAll("button");
+const scoreBoard = document.querySelector("#score");
+console.log(scoreBoard);
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (humanSelection) => {
+        for(let i = 1; i <= 3; i++) {
+            if(btn.classList.contains(`btn-${i}`)) {
+                humanSelection = i - 1;
+                break;
+            }
+        }
+        console.log(humanSelection);
+        playRound(humanSelection);
+        console.log(humanScore + ' : ' + computerScore);
+        scoreBoard.textContent = humanScore + ' : ' + computerScore;
+    });
+});
