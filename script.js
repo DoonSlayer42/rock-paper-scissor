a595a59d1ce77a36e5fa2ae436067fea6585fbfe
DoonSlayer42 <internetsaathihwgo6040@gmail.com>
2024-09-28 20:02:44 +0530

    Revert "Added the logic for the game along with some modularization using functions."
    This reverts commit 6d05485354faf075329a7129124eae6c46f8ff44.

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let choice = prompt("Enter your choice (rock, paper, scissor): ");
-    choice = choice.toLowerCase();
+    let choice = prompt("Enter your choice (rock, paper, scissor): ", "rock");
     if(choice === "rock") {
         return 0;
     }
@@ -19,48 +18,6 @@ function getHumanChoice() {
     return 2;
 }
 
+console.log(getHumanChoice());
 
-function playRound(humanSelection, computerSelection) {
-    if(humanSelection === 0) {
-        if(computerSelection === 1) {
-            computerScore++;
-        }
-        else if(computerSelection === 2) {
-            humanScore++;
-        }
-    }
-    else if(humanSelection === 1) {
-        if(computerSelection === 0) {
-            humanScore++;
-        }
-        else if(computerSelection === 2) {
-            computerScore++;
-        }
-    }
-    else {
-        if(computerSelection === 0) {
-            computerScore++;
-        }
-        else if(computerSelection === 1) {
-            humanScore++;
-        }
-    }
-}
-
-for(let i = 0; i < 5; i++) {
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-}
-
-console.log(humanScore + ' : ' + computerScore);
-
-if(humanScore > computerScore) {
-    console.log("You won!");
-}
-else if(humanScore < computerScore) {
-    console.log("Game over!");
-}
-else {
-    console.log("It's a draw!");
-}
+// let userResponse = prompt("Enter your choice (rock, paper or scissor): ", "rock");
